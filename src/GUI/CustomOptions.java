7b956fca9ccb47ab4d9c3d51b852841c569139a1
Nutil <luis.rpfigueiredo@gmail.com>
2016-05-27 15:32:55 +0100

    Fixed commit

---
+++ src/GUI/CustomOptions.java
@@ -0,0 +1,38 @@
+package GUI;
+
+//TODO change to singleton design pattern
+public class CustomOptions {
+    private String algorithm;
+    private int numberTribunals;
+    private int budget;
+
+    public CustomOptions(){
+        algorithm = "genetic";
+        numberTribunals = 50;
+        budget = 50;
+    }
+
+    public int getNumberTribunals() {
+        return numberTribunals;
+    }
+
+    public void setNumberTribunals(int numberTribunals) {
+        this.numberTribunals = numberTribunals;
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+}
